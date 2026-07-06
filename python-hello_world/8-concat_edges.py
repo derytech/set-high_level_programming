1e1b281c18cca6360df38d8a062406929866b916
derytech <samuelaanyemdery@gmail.com>
2026-07-06 08:14:04 -0700

    Fix string slicing output

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:53] + str[55:66] + str[1:3] + str[:6])
