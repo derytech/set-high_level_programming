b83954db3497c157ca1cc7728cd0547f1de3b05f
derytech <samuelaanyemdery@gmail.com>
2026-07-13 10:18:49 -0700

    Print alphabet in reverse alternating cases

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+result = ""
+for i in range(122, 96, -1):
+    if (122 - i) % 2 == 0:
+        result += chr(i)
+    else:
+        result += chr(i - 32)
+
+print("{}".format(result), end="")
