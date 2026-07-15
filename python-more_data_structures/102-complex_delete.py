7bae1efa90720fa1a82e52aff4b48eb9ac714548
derytech <samuelaanyemdery@gmail.com>
2026-07-15 09:44:30 -0700

    Solve task 102: Complex delete

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    """Delete all keys with a specific value."""
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+
+    return a_dictionary
