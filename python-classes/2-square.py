2e63d34449cfd02176a4953ba3769c1d3f0de1c5
derytech <samuelaanyemdery@gmail.com>
2026-07-20 18:34:35 -0700

    Fix pycodestyle and documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """This module defines a Square class."""
 
@@ -7,15 +6,6 @@ class Square:
     """A class that defines a square."""
 
     def __init__(self, size=0):
-        """Initialize a square.
-
-        Args:
-            size (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
