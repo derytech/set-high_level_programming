4aa3b96db1a095d9155e1239eda833efe45fe685
derytech <samuelaanyemdery@gmail.com>
2026-07-20 18:30:29 -0700

    Add validation for Square size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,61 +1,23 @@
-Write a class Square that defines a square by: (based on 1-square.py)
 
-Private instance attribute: size
-Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
-You are not allowed to import any module
-guillaume@ubuntu:~/0x06$ cat 2-main.py
 #!/usr/bin/python3
-Square = __import__('2-square').Square
+"""This module defines a Square class."""
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+class Square:
+    """A class that defines a square."""
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """Initialize a square.
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+        Args:
+            size (int): The size of the square.
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
-
-guillaume@ubuntu:~/0x06$ ./2-main.py
-<class '2-square.Square'>
-{'_Square__size': 3}
-<class '2-square.Square'>
-{'_Square__size': 0}
-'Square' object has no attribute 'size'
-'Square' object has no attribute '__size'
-size must be an integer
-size must be >= 0
-guillaume@ubuntu:~/0x06$ 
-
-EW_Lesson Footer.png
-
-Repo:
-
-GitHub repository: set-high_level_programming
-Directory: python-classes
-File: 2-square.py
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
