35af6d020fa4bd76eff9319102fc5f37c853e489
derytech <samuelaanyemdery@gmail.com>
2026-08-06 16:44:00 -0700

    Add 3-say_my_name.py and doctest suite

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains the `say_my_name` function.
+
+The function prints 'My name is <first name> <last name>'.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints a full name in the format 'My name is <first name> <last name>'.
+
+    Args:
+        first_name: First name (must be a string).
+        last_name: Last name (must be a string, defaults to empty string).
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
