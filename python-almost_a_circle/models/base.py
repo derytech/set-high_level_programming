87ba07a37c895ab8f2cc1529d1917218c64a1cc1
derytech <samuelaanyemdery@gmail.com>
2026-08-13 15:34:03 -0700

    Add JSON static methods to Base and fix test suite

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
 Module for Base class.
+Defines the base model for all other classes in the project.
 """
+import json
 
 
 class Base:
@@ -16,3 +18,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation json_string."""
+        if json_string is None or json_string == "" or json_string == "[]":
+            return []
+        return json.loads(json_string)
