331b32bd3384f2759dd98cc6374748f59f0b8c9c
derytech <samuelaanyemdery@gmail.com>
2026-08-13 15:42:02 -0700

    Implement Rectangle class and update Base class methods

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Module for Base class.
 Defines the base model for all other classes in the project.
 """
 import json
+import os
 
 
 class Base:
@@ -26,9 +27,41 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
+        filename = f"{cls.__name__}.json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                f.write(Base.to_json_string(list_dicts))
+
     @staticmethod
     def from_json_string(json_string):
         """Return the list of the JSON string representation json_string."""
         if json_string is None or json_string == "" or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                dummy = cls(1, 1)
+            else:
+                dummy = cls(1)
+            dummy.update(**dictionary)
+            return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances from a file."""
+        filename = f"{cls.__name__}.json"
+        if not os.path.exists(filename):
+            return []
+        with open(filename, "r") as f:
+            list_dicts = Base.from_json_string(f.read())
+            return [cls.create(**d) for d in list_dicts]
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""
+Module for Rectangle class.
+Inherits from Base and manages rectangle attributes.
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent a Rectangle that inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle instance."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get or set the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get or set the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get or set the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get or set the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the Rectangle instance."""
+        return self.__width * self.__height
+
+    def display(self):
+        """Print the Rectangle instance using the '#' character."""
+        print("\n" * self.__y, end="")
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """Return string representation of the Rectangle instance."""
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
+
+    def update(self, *args, **kwargs):
+        """Update attributes of the Rectangle instance."""
+        attrs = ["id", "width", "height", "x", "y"]
+        if args and len(args) != 0:
+            for i, arg in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
