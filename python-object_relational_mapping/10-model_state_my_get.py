68eb751b5f84b59207a802e65d6a58091ea0e89b
derytech <samuelaanyemdery@gmail.com>
2026-08-17 16:24:29 -0700

    Add script to get state by name

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Print the ID of a State object matching a given name."""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    session = Session(engine)
+
+    state = session.query(State).filter(
+        State.name == sys.argv[4]
+    ).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    session.close()
