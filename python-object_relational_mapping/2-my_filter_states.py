3358c066e2ca1f2e86728590daaad6ae240d3e47
derytech <samuelaanyemdery@gmail.com>
2026-08-17 15:22:39 -0700

    Add script to filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Display states matching a given name."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        sys.argv[4]
+    )
+    cursor.execute(query)
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    db.close()
