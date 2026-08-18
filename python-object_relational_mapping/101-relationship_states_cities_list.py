4152649c4a2afdece06cf59bc868313244223ade
derytech <samuelaanyemdery@gmail.com>
2026-08-17 17:27:25 -0700

    Fix state city relationship listing

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-"""List all states and their cities using SQLAlchemy relationships."""
+"""List states and their cities using SQLAlchemy relationships."""
 
-from relationship_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+
+from relationship_state import State
 
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
+        )
     )
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    states = (
+        session.query(State)
+        .options(joinedload(State.cities))
+        .order_by(State.id)
+        .all()
+    )
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
+        for city in sorted(state.cities, key=lambda city: city.id):
             print("\t{}: {}".format(city.id, city.name))
 
     session.close()
