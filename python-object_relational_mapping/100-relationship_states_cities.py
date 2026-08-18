640e19bafce4ee7e3d600f241e5fe04304f41b18
derytech <samuelaanyemdery@gmail.com>
2026-08-17 17:38:43 -0700

    Add relationship_city, relationship_state, and 100-relationship_states_cities script

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-"""Create a California state and its San Francisco city."""
-
+"""
+Creates the State "California" with the City "San Francisco"
+from the database hbtn_0e_100_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
         pool_pre_ping=True
     )
 
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    state = State(name="California")
-    city = City(name="San Francisco")
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
 
-    state.cities.append(city)
-
-    session.add(state)
+    session.add(new_state)
     session.commit()
-
     session.close()
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""State class with a relationship to City."""
-
+"""
+Contains the class definition of a State and an instance Base.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-from model_state import Base
+Base = declarative_base()
 
 
 class State(Base):
-    """State class."""
+    """
+    State class that inherits from Base and links to the MySQL table states.
+    """
+    __tablename__ = 'states'
 
-    __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
     cities = relationship(
-        "City",
-        back_populates="state",
-        cascade="all, delete-orphan"
+        "City", backref="state", cascade="all, delete-orphan"
     )
