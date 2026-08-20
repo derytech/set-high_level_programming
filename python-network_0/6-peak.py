4b85fec19c06d44f14eff6a64c161aa7e4f47c42
derytech <samuelaanyemdery@gmail.com>
2026-08-20 11:16:22 -0700

    Add 6-peak.py and 6-peak.txt implementing peak finding algorithm

---
+++ python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module containing the find_peak function
+"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers using binary search."""
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
